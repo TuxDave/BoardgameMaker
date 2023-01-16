28ee97191de10d0fe58dbbc50b0bc5464c0bf127
TuxDave <godinodavide@gmail.com>
2023-01-16 20:01:00 +0100

    created setup list of actions

--- Core/src/main/kotlin/it/spaghetticode/bgm/core/Game.kt
+++ Core/src/main/kotlin/it/spaghetticode/bgm/core/Game.kt
@@ -2,6 +2,7 @@ package it.spaghetticode.bgm.core
 
 import it.spaghetticode.bgm.core.game.nodes.Folder
 import it.spaghetticode.bgm.core.game.nodes.Node
+import it.spaghetticode.bgm.core.game.logic.Action
 import it.spaghetticode.bgm.core.utils.IntRangeSerializer
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
@@ -10,21 +11,25 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.lang.Integer.max
 
+fun MutableList<Action>.execute(): Unit {
+    this.forEach{
+        it.execute()
+    }
+}
+
 class Game(
     playerRange: IntRange = 1 .. 1,
-    val structure: Node = Folder()
-    // TODO: add the list of action (setup)
-    //create an extension function for a list of Action to execute each in sequence
-    //decide how to manage the setup (mutableList) because could create some problems in deserialization (different instance of the same object ecc)
+    val structure: Node = Folder(),
+    val setup: MutableList<Action> = mutableListOf()
 ){
     var playerRange: IntRange = 1 .. 1
         set(value) {
             field = if(value.first > 1 && value.last >= value.first){
                 value
             }else{
-                var start = max(value.first, 1)
-                var end = max(value.last, start)
-                start .. end
+                val start = max(value.first, 1)
+//                val end = max(value.last, start)
+                start .. max(value.last, start)
             }
         }
 
@@ -34,7 +39,8 @@ class Game(
 
     internal constructor(gameSerializable: GameSerializable) : this(
         playerRange = gameSerializable.playerRange,
-        structure = gameSerializable.structure
+        structure = gameSerializable.structure,
+        setup = gameSerializable.setup
     )
 }
 
@@ -42,7 +48,8 @@ class Game(
 internal data class GameSerializable(
     @Serializable(with = IntRangeSerializer::class)
     val playerRange: IntRange,
-    val structure: Node //this must be Deserialized before other structures using the same nodes as refer
+    val structure: Node, //this must be Deserialized before other structures using the same nodes as refer
+    val setup: MutableList<Action>
 )
 
 class GameSerializer: KSerializer<Game>{
@@ -50,7 +57,8 @@ class GameSerializer: KSerializer<Game>{
     override fun serialize(encoder: Encoder, value: Game) {
         val gameSerializable = GameSerializable(
             value.playerRange,
-            value.structure
+            value.structure,
+            value.setup
         )
         encoder.encodeSerializableValue(GameSerializable.serializer(), gameSerializable)
     }
