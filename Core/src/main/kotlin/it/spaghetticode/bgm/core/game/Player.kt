6790212f25812d9cc1237dc785bebd469bd28f36
TuxDave <godinodavide@gmail.com>
2023-01-18 19:54:45 +0100

    create (but void) Player class

---
+++ Core/src/main/kotlin/it/spaghetticode/bgm/core/game/Player.kt
@@ -0,0 +1,9 @@
+package it.spaghetticode.bgm.core.game
+
+import it.spaghetticode.bgm.core.annotations.GameData
+
+//NOT FUCKING SERIALIZABLE
+class Player(
+    @GameData var name: String
+    // TODO: create role? class ecc 
+)
