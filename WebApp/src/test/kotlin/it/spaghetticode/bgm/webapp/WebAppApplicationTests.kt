1a7dec1d36754d357af3a64269724e4af9683a1b
TuxDave <godinodavide@gmail.com>
2023-01-20 11:47:54 +0100

    added webapp code base

---
+++ WebApp/src/test/kotlin/it/spaghetticode/bgm/webapp/WebAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.spaghetticode.bgm.webapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
