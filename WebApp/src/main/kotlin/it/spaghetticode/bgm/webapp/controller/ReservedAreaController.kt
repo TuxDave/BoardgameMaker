55cf3faabc400cf8eff81b5709be63feb44863a7
TuxDave <godinodavide@gmail.com>
2023-01-30 19:25:49 +0100

    game edit works

--- WebApp/src/main/kotlin/it/spaghetticode/bgm/webapp/controller/ReservedAreaController.kt
+++ WebApp/src/main/kotlin/it/spaghetticode/bgm/webapp/controller/ReservedAreaController.kt
@@ -7,9 +7,11 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.multipart.MultipartFile
 
 @Controller
 @RequestMapping("reserved/")
@@ -76,5 +78,61 @@ class ReservedAreaController {
         }
         // TODO: finish to refine 
         return "redirect:/reserved/overview"
+    }
+
+    @GetMapping("edit-game")
+    fun editGamePage(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        @RequestParam("gameId") gameId: Long
+    ): String {
+        val user = userService.findById(request.session.getAttribute("userId") as Long? ?: -1) ?: return "redirect:/login"
+
+        val game = gameService.findById(gameId)
+        game?.let {
+            if(game.admin == user){
+                request.setAttribute("title","Edit Game")
+                request.setAttribute("content","contents/reserved_area/game_edit")
+                println(game.description)
+                request.session.setAttribute("editingGameId", game.id)
+
+                request.setAttribute("game", game)
+                return "pages/base"
+            } else return "redirect:/reserved/overview"
+        } ?: return "redirect:/reserved/overview"
+    }
+
+    @PostMapping("edit-game")
+    fun editGameAction(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        @RequestParam name: String,
+        @RequestParam description: String,
+        @RequestParam blob: MultipartFile?
+    ): String {
+        val user = userService.findById(request.session.getAttribute("userId") as Long? ?: -1) ?: return "redirect:/login"
+
+        val game = gameService.findById(request.session.getAttribute("editingGameId") as Long)
+        return game?.let {
+            var edit = false
+            if(game.name != name && name != ""){
+                game.name = name
+                edit = true
+            }
+            if(description != game.description){
+                game.description = description
+                edit = true
+            }
+            blob?.let {
+                //TODO: check if is a ZIP within a game inside
+                game.gameData = blob.bytes
+                edit = true
+            }
+            if(edit){
+                gameService.save(game)
+                request.session.removeAttribute("editingGameId")
+            }
+            return "redirect:/reserved/overview"
+        } ?: "redirect:/reserved/overview"
     }
 }
